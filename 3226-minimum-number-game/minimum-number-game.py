be7bf2ee13120a9792c13903b6714fc0cfb2133c
Bishesh Pokharel <77426923+Bishesh-P@users.noreply.github.com>
2026-08-11 11:08:15 +0545

    Time: 7 ms (8.01%) | Memory: 19.3 MB (60.86%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        arr = []
+        while nums:
+            alice = min(nums)
+            nums.remove(alice)
+            bob = min(nums)
+            nums.remove(bob)
+            arr.append(bob)
+            arr.append(alice)
+        return arr
+        
