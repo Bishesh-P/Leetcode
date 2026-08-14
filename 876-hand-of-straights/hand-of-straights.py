7c76336ce65226dac4cb5c6c2f78cd9a0a0ec105
Bishesh Pokharel <77426923+Bishesh-P@users.noreply.github.com>
2026-08-14 11:09:14 +0545

    Time: 31 ms (66.59%) | Memory: 20.8 MB (99.09%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,18 +1,23 @@
-from collections import Counter
-
 class Solution:
     def isNStraightHand(self, hand, groupSize):
         if len(hand) % groupSize != 0:
             return False
 
-        count = Counter(hand)
+        # HashMap: count frequency
+        count = {}
 
+        for card in hand:
+            count[card] = count.get(card, 0) + 1
+
+        # Process cards from smallest to largest
         for card in sorted(count):
+
             if count[card] > 0:
                 start_count = count[card]
 
                 for x in range(card, card + groupSize):
-                    if count[x] < start_count:
+
+                    if x not in count or count[x] < start_count:
                         return False
 
                     count[x] -= start_count
